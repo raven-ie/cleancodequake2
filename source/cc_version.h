84b620c2df60057cde5099830b10efc3805b96ab
jonno.5000 <jonno.5000@gmail.com>
2009-12-12 04:53:38 +0000

    Point Release 1 preparations

--- source/cc_version.h
+++ source/cc_version.h
@@ -34,10 +34,10 @@ list the mod on my page for CleanCode Quake2 to help get the word around. Thanks
 #if !defined(__CC_VERSION_H__) || !INCLUDE_GUARDS
 #define __CC_VERSION_H__
 
-#define CLEANCODE_VERSION_PREFIX	"rc"
-#define CLEANCODE_VERSION_MAJOR_N	0			// x
+#define CLEANCODE_VERSION_PREFIX	"pr"
+#define CLEANCODE_VERSION_MAJOR_N	1			// x
 #define CLEANCODE_VERSION_MINOR_N	0			// xxxx
-#define CLEANCODE_VERSION_BUILD_N	251			// xxxx
+#define CLEANCODE_VERSION_BUILD_N	253			// xxxx
 
 #define CLEANCODE_VERSION_PRINT			"\"%s.%u.%04u.%05u\""
 #define CLEANCODE_VERSION_PRINT_ARGS	CLEANCODE_VERSION_PREFIX, CLEANCODE_VERSION_MAJOR_N, CLEANCODE_VERSION_MINOR_N, CLEANCODE_VERSION_BUILD_N
